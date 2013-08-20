36f55cabd442517130159945ed8dd6bd722dd41f
Sune Simonsen <sune@we-knowhow.dk>
2013-08-20 21:33:18 +0200

    Added assertion 'was [not] called'

--- test/unexpected.sinon.spec.js
+++ test/unexpected.sinon.spec.js
@@ -1,3 +1,29 @@
 describe('unexpected.sinon', function () {
-    
+    describe('was called assertion', function () {
+        it('asserts that a spy was called', function () {
+            var spy = sinon.spy();
+            spy();
+            expect(spy, "was called");
+        });
+
+        it('fails if spy was not called', function () {
+            expect(function () {
+                expect(sinon.spy(), "was called");
+            }, "to throw exception", "expected spy to have been called at least once but was never called");
+        });
+    });
+
+    describe('was not called assertion', function () {
+        it('asserts that a spy was not called', function () {
+            expect(sinon.spy(), "was not called");
+        });
+
+        it('fails if spy was called', function () {
+            expect(function () {
+                var spy = sinon.spy();
+                spy();
+                expect(spy, "was not called");
+            }, "to throw exception", /expected spy to not have been called but was called once/);
+        });
+    });
 });
