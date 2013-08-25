f69b08df22e0c50b23c2035615627b072bf32b4a
Sune Simonsen <sune@we-knowhow.dk>
2013-08-25 22:55:54 +0200

    Added 'was called with exactly' assertion

--- test/unexpected.sinon.spec.js
+++ test/unexpected.sinon.spec.js
@@ -227,8 +227,17 @@ describe('unexpected.sinon', function () {
     });
 
     describe('was called with exactly', function () {
-        it('passes if the spy was called with the provided arguments and no others');
-        it('fails if the spy was never called with the provided arguments and no others');
+        it('passes if the spy was called with the provided arguments and no others', function () {
+            spy('foo', 'bar', 'baz');
+            expect(spy, 'was called with exactly', 'foo', 'bar', sinon.match.truthy);
+        });
+
+        it('fails if the spy was never called with the provided arguments and no others', function () {
+            expect(function () {
+                spy('foo', 'bar', 'baz', 'qux');
+                expect(spy, 'was called with exactly', 'foo', 'bar', sinon.match.truthy);
+            }, 'to throw exception', /spy\(foo, bar, baz, qux\)/);
+        });
     });
 
     describe('was always called with exactly', function () {
