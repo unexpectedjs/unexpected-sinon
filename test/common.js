a2d1537d4207019af5b148f440f1c80526251f3f
Sune Simonsen <sune@we-knowhow.dk>
2015-05-09 20:54:36 +0200

    Updated site

--- test/common.js
+++
@@ -1,5 +0,0 @@
-sinon = require('sinon');
-expect = require('unexpected');
-unexpectedSinon = process.env.COVERAGE ?
-    require('../lib-cov/unexpected-sinon') :
-    require('../lib/unexpected-sinon');
