80fc505557d907a3c016627b4c6f61edd1e84090
Andreas Lind <andreas@one.com>
2015-09-01 09:13:01 +0200

    test/common: Remove no longer used lib-cov support.

--- test/common.js
+++ test/common.js
@@ -1,7 +1,5 @@
 /*global sinon:true, unexpected:true, unexpectedSinon:true*/
 sinon = require('sinon');
 unexpected = require('unexpected').clone();
-unexpectedSinon = process.env.COVERAGE ?
-    require('../lib-cov/unexpected-sinon') :
-    require('../lib/unexpected-sinon');
+unexpectedSinon = require('../lib/unexpected-sinon');
 unexpected.installPlugin(unexpectedSinon);
