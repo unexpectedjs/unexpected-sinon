f34bf26fda1cc05762c88617799807fda859dbeb
Andreas Lind <andreas@one.com>
2015-09-04 18:47:40 +0200

    It's a bit hard to support the omitSubject feature with 'always threw' currently, adjust the test.

--- test/unexpected-sinon.spec.js
+++ test/unexpected-sinon.spec.js
@@ -543,8 +543,7 @@ describe('unexpected-sinon', function () {
                        "\n" +
                        "invocations(\n" +
                        "  stub() at theFunction (theFileName:xx:yy),\n" +
-                       "  stub() at theFunction (theFileName:xx:yy) // expected: threw { name: 'Error' }\n" +
-                       "                                            //   expected TypeError() to satisfy { name: 'Error' }\n" +
+                       "  stub() at theFunction (theFileName:xx:yy) // expected stub() at theFunction (theFileName:xx:yy) threw { name: 'Error' }\n" +                       "                                            //   expected TypeError() to satisfy { name: 'Error' }\n" +
                        "                                            //\n" +
                        "                                            //   {\n" +
                        "                                            //     message: '',\n" +
