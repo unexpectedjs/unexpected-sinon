98d26866f6e8b3ee722f6a5a47290a5b3f3afa09
Andreas Lind <andreas@one.com>
2016-01-26 23:44:12 +0100

    Update unexpected-documentation-site-generator.
    Remove the checked-in documentation tests.

--- test/common.js
+++
@@ -1,7 +0,0 @@
-/*global sinon:true, unexpected:true, unexpectedSinon:true*/
-sinon = require('sinon');
-unexpected = require('unexpected').clone();
-unexpectedSinon = require('../lib/unexpected-sinon');
-unexpected.installPlugin(unexpectedSinon);
-
-require('./monkeyPatchSinonStackFrames')(sinon);
