b9117e9cd04fe79b3efc18604a393ec83f4790d2
Andreas Lind <andreas@one.com>
2016-04-07 22:32:10 +0200

    Fix stack mocking with Phantom.js.
    Switch to a Sinon.js fork that is able to generate stack traces with Phantom.js

--- test/monkeyPatchSinonStackFrames.js
+++ test/monkeyPatchSinonStackFrames.js
@@ -2,16 +2,32 @@
 // so that the top stack frame is a predictable string.
 // Prevents every test from failing when the test suite is updated.
 module.exports = function (sinon) {
-    var sinonCreate = sinon.create;
-    sinon.create = function (arg) {
-        var getStackFrames = arg.getStackFrames;
+    // Copied from test/monkeyPatchSinonStackFrames.js
+    function patchCall(call) {
+        var getStackFrames = call && call.getStackFrames;
         if (getStackFrames) {
-            arg.getStackFrames = function () {
+            call.getStackFrames = function () {
                 var stackFrames = getStackFrames.call(this);
                 stackFrames[0] = 'at theFunction (theFileName:xx:yy)';
                 return stackFrames;
             };
         }
-        return sinonCreate.call(this, arg);
-    };
+        return call;
+    }
+
+    ['spy', 'stub'].forEach(function (name) {
+        var orig = sinon[name];
+        sinon[name] = function () {
+            var result = orig.apply(this, arguments);
+            var getCall = result.getCall;
+            result.getCall = function () {
+                return patchCall(getCall.apply(result, arguments));
+            };
+            var getCalls = result.getCalls;
+            result.getCalls = function () {
+                return getCalls.call(result).map(patchCall);
+            };
+            return result;
+        };
+    });
 };
