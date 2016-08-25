3cfd6683c76d1cbef068446ea4849c2606a07695
Sune Simonsen <sune@we-knowhow.dk>
2016-08-25 21:43:27 +0200

    Fixing broken build

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,6 +1,8 @@
-/*global unexpected:true, sinon:true*/
+/*global unexpected:true, sinon:true, Promise:true*/
+/* exported Promise */
 unexpected = require('unexpected');
 unexpected.output.preferredWidth = 80;
 sinon = require('sinon');
 unexpected.installPlugin(require('./lib/unexpected-sinon'));
 require('./test/monkeyPatchSinonStackFrames')(sinon);
+Promise = require('rsvp');
