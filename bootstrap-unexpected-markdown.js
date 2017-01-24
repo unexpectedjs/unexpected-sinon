a65387a29a1e903a0f73c99e259eaa12958bae9d
Andreas Lind <andreas@one.com>
2017-01-24 15:42:59 +0100

    Unbreak documentation tests.

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,7 +1,8 @@
-/*global unexpected:true, Promise:true*/
-/* exported Promise */
+/*global unexpected:true, sinon, Promise:true*/
+/* exported Promise, sinon */
 unexpected = require('unexpected');
 unexpected.output.preferredWidth = 80;
 unexpected.installPlugin(require('./lib/unexpected-sinon'));
 require('./test/monkeyPatchSinonStackFrames');
 Promise = require('rsvp');
+sinon = require('sinon');
