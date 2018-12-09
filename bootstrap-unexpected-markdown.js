6595fbd7f71c1d0da91ab081b0591acc2edd1271
Andreas Lind <andreaslindpetersen@gmail.com>
2018-12-09 21:29:52 +0100

    eslint --fix .

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -5,6 +5,6 @@ unexpected.output.preferredWidth = 80;
 unexpected.installPlugin(require('./lib/unexpected-sinon'));
 require('./test/monkeyPatchSinonStackFrames');
 if (typeof Promise === 'undefined') {
-    Promise = require('rsvp').Promise;
+  Promise = require('rsvp').Promise;
 }
 sinon = require('sinon');
