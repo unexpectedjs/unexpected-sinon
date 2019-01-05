0e67c2e7fa72e0f577b14c7a347d1fe425fcd831
Alex J Burke <alex@alexjeffburke.com>
2019-01-05 20:19:01 +0100

    Replace phantomjs with karma.

---
+++ karma.conf.js
@@ -0,0 +1,26 @@
+var puppeteer = require('puppeteer');
+process.env.CHROME_BIN = puppeteer.executablePath();
+
+module.exports = function(config) {
+    config.set({
+        frameworks: ['mocha'],
+
+        files: [
+            './node_modules/unexpected/unexpected.js',
+            './node_modules/sinon/pkg/sinon.js',
+            './lib/unexpected-sinon.js',
+            './test/browser/common.js',
+            './test/monkeyPatchSinonStackFrames.js',
+            './test/unexpected-sinon.spec.js'
+        ],
+
+        client: {
+            mocha: {
+                reporter: 'html',
+                timeout: 60000
+            }
+        },
+
+        browsers: ['ChromeHeadless']
+    });
+};
